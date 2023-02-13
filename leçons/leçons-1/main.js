6611e68934f2572f816ff881df037ac434a09a12
unknown <fyoudom@yahoo.com>
2023-02-13 16:27:23 +0100

    premier cour de javascript

---
+++ leçons/leçons-1/main.js
@@ -0,0 +1,4 @@
+alert('hi people, Good morning and welcome to JS Beguiner-pro');
+        // alert('hello please what is your name ?')
+        var receiveName = prompt('please what is your name ?');
+        alert('welcome ' + receiveName);
